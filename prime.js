f13a9802d993fccebb03c3c88817feba6dc1bd35
Ashish0076 <ashish9422301@gmail.com>
2022-11-30 12:53:32 +0530

    code_updated

--- prime.js
+++ prime.js
@@ -1,13 +1,13 @@
 
 let num = 13;
-let count=1;     // update : count =0;
+let count=0;     // update : count =0;
 
 for ( let i=1; i<=num; i++ ){   
-  if( num % i === 0 ){
+  if( num % i/2 === 0 ){     //update : num%i/2==0
     count++;
   }
 }
-if( count==2 ){
+if( count==1 ){      //update : count==0;
   console.log("Yes");
 }else {
   console.log("No")
